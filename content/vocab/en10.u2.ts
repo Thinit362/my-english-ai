7818a24d7667bf3157050a8c723e6a7d5e8508a7
Thinit362 <133291441+Thinit362@users.noreply.github.com>
2025-11-09 20:38:25 +0700

    Update en10.u2.ts

--- content/vocab/en10.u2.ts
+++ content/vocab/en10.u2.ts
@@ -1,3 +1,8 @@
 import type { UnitVocab } from "../english10.vocab";
-const data: UnitVocab = { unitId: 2, title: "Unit 2 – (updating)", items: [] };
+const data: UnitVocab = {
+  unitId: 2,
+  unitTitle: "Unit 2",
+  lessons: []
+};
+
 export default data;
