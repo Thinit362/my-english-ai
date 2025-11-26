1baf07778430c54627a2688dbe5f99f3a364a8e5
Thinit362 <133291441+Thinit362@users.noreply.github.com>
2025-11-26 20:58:55 +0700

    Update PracticeGameRenderer.tsx

--- components/PracticeGameRenderer.tsx
+++ components/PracticeGameRenderer.tsx
@@ -21,7 +21,8 @@ const PracticeGameRenderer: React.FC<Props> = ({ task }) => {
   switch (task.gameType) {
     case "flash-audio-match":
       return <FlashAudioMatchGame datasetId={task.datasetId} />;
-
+    case "drag-fill":
+      return <DragFillGame datasetId={task.datasetId} />;
     default:
       return (
         <div className="p-4 text-slate-600">
