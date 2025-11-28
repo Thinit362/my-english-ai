41e84819db401de3ad2debddced1dd410800e82f
Thinit362 <133291441+Thinit362@users.noreply.github.com>
2025-11-28 22:17:04 +0700

    Create en10.u1.p1.ex1.ts

---
+++ content/practice/listenChoose/en10.u1.p1.ex1.ts
@@ -0,0 +1,94 @@
+import { ListenChooseDataset } from "@/components/games/ListenChooseGame";
+
+export const en10u1p1ex1: ListenChooseDataset = {
+  id: "en10.u1.p1.ex1",
+  title: "Bài tập 1: Nghe và chọn đáp án đúng.",
+  items: [
+    {
+      id: "q1",
+      audio: "/audio/u1/p1/q1.mp3",
+      question: "The man took out a handkerchief to mop his ______.",
+      optionA: "brow",
+      optionB: "blow",
+      correct: "A",
+    },
+    {
+      id: "q2",
+      audio: "/audio/u1/p1/q2.mp3",
+      question:
+        "It is considered ______ and insensitive to ask the disabled such questions.",
+      optionA: "class",
+      optionB: "crass",
+      correct: "B",
+    },
+    {
+      id: "q3",
+      audio: "/audio/u1/p1/q3.mp3",
+      question:
+        "I found it hard to concentrate while hearing the loud ______ of the typewriter.",
+      optionA: "crack",
+      optionB: "clack",
+      correct: "B",
+    },
+    {
+      id: "q4",
+      audio: "/audio/u1/p1/q4.mp3",
+      question: "The state officer has become ______ about the affair.",
+      optionA: "trunk",
+      optionB: "chunk",
+      correct: "B",
+    },
+    {
+      id: "q5",
+      audio: "/audio/u1/p1/q5.mp3",
+      question: "At the party, one of the games was to ______ bubbles.",
+      optionA: "blow",
+      optionB: "brow",
+      correct: "A",
+    },
+    {
+      id: "q6",
+      audio: "/audio/u1/p1/q6.mp3",
+      question:
+        "To ______ almonds, you need to remove the outer skin first.",
+      optionA: "blanch",
+      optionB: "branch",
+      correct: "A",
+    },
+    {
+      id: "q7",
+      audio: "/audio/u1/p1/q7.mp3",
+      question: "The little boy wanted one more ______ of cheese.",
+      optionA: "trunk",
+      optionB: "chunk",
+      correct: "B",
+    },
+    {
+      id: "q8",
+      audio: "/audio/u1/p1/q8.mp3",
+      question:
+        '"How wonderful!" she ______ as she reached the destination.',
+      optionA: "chilled",
+      optionB: "trilled",
+      correct: "B",
+    },
+    {
+      id: "q9",
+      audio: "/audio/u1/p1/q9.mp3",
+      question:
+        "What he said today is just going to ______ the conflict.",
+      optionA: "trigger",
+      optionB: "jigger",
+      correct: "A",
+    },
+    {
+      id: "q10",
+      audio: "/audio/u1/p1/q10.mp3",
+      question:
+        "He now has to rely on a ______. One of his legs was broken.",
+      optionA: "clutch",
+      optionB: "crutch",
+      correct: "B",
+    },
+  ],
+};
