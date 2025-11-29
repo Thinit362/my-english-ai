f14849f6ba0e6deb8f3a317903aabe95c802a12b
Thinit362 <133291441+Thinit362@users.noreply.github.com>
2025-11-29 08:54:45 +0700

    Create datasets.ts

---
+++ content/practice/listenChoose/datasets.ts
@@ -0,0 +1,7 @@
+// content/practice/listenChoose/datasets.ts
+import { en10u1p1ex1 } from "./en10.u1.p1.ex1";
+
+// đơn giản dùng any cho đỡ rắc rối type
+export const LISTEN_CHOOSE_DATASETS: Record<string, any> = {
+  "en10.u1.p1.ex1": en10u1p1ex1,
+};
