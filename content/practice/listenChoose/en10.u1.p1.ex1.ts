c451f88f2099fcecc435897a9a97e49229fe9b50
Thinit362 <133291441+Thinit362@users.noreply.github.com>
2025-11-29 08:53:07 +0700

    Update en10.u1.p1.ex1.ts

--- content/practice/listenChoose/en10.u1.p1.ex1.ts
+++ content/practice/listenChoose/en10.u1.p1.ex1.ts
@@ -1,6 +1,5 @@
-import { ListenChooseDataset } from "@/components/games/ListenChooseGame";
-
-export const en10u1p1ex1: ListenChooseDataset = {
+// content/practice/listenChoose/en10.u1.p1.ex1.ts
+export const en10u1p1ex1 = {
   id: "en10.u1.p1.ex1",
   title: "Bài tập 1: Nghe và chọn đáp án đúng.",
   items: [
@@ -41,7 +40,8 @@ export const en10u1p1ex1: ListenChooseDataset = {
     {
       id: "q5",
       audio: "/audio/u1/p1/q5.mp3",
-      question: "At the party, one of the games was to ______ bubbles.",
+      question:
+        "One of the games held at the party was to ______ bubbles.",
       optionA: "blow",
       optionB: "brow",
       correct: "A",
@@ -49,8 +49,7 @@ export const en10u1p1ex1: ListenChooseDataset = {
     {
       id: "q6",
       audio: "/audio/u1/p1/q6.mp3",
-      question:
-        "To ______ almonds, you need to remove the outer skin first.",
+      question: "To ______ almonds, you need to remove the outer skin first.",
       optionA: "blanch",
       optionB: "branch",
       correct: "A",
@@ -66,8 +65,7 @@ export const en10u1p1ex1: ListenChooseDataset = {
     {
       id: "q8",
       audio: "/audio/u1/p1/q8.mp3",
-      question:
-        '"How wonderful!" she ______ as she reached the destination.',
+      question: '"How wonderful!" she ______ as she reached the destination.',
       optionA: "chilled",
       optionB: "trilled",
       correct: "B",
@@ -76,7 +74,7 @@ export const en10u1p1ex1: ListenChooseDataset = {
       id: "q9",
       audio: "/audio/u1/p1/q9.mp3",
       question:
-        "What he said today is just going to ______ the conflict.",
+        "What he said today is just going to ______ the conflict between the two teams.",
       optionA: "trigger",
       optionB: "jigger",
       correct: "A",
@@ -85,7 +83,7 @@ export const en10u1p1ex1: ListenChooseDataset = {
       id: "q10",
       audio: "/audio/u1/p1/q10.mp3",
       question:
-        "He now has to rely on a ______. One of his legs was broken.",
+        "He now has to rely on a ______. One of his legs was broken two weeks ago.",
       optionA: "clutch",
       optionB: "crutch",
       correct: "B",
