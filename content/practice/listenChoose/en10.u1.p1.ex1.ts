58859548c6be76786e7b1191d42955a17bf44309
Thinit362 <133291441+Thinit362@users.noreply.github.com>
2025-11-30 21:44:46 +0700

    Update en10.u1.p1.ex1.ts

--- content/practice/listenChoose/en10.u1.p1.ex1.ts
+++ content/practice/listenChoose/en10.u1.p1.ex1.ts
@@ -1,11 +1,11 @@
 // content/practice/listenChoose/en10.u1.p1.ex1.ts
+
 export const en10u1p1ex1 = {
   id: "en10.u1.p1.ex1",
   title: "Bài tập 1: Nghe và chọn đáp án đúng.",
   items: [
     {
       id: "q1",
-      audio: "/audio/u1/p1/q1.mp3",
       question: "The man took out a handkerchief to mop his ______.",
       optionA: "brow",
       optionB: "blow",
@@ -13,7 +13,6 @@ export const en10u1p1ex1 = {
     },
     {
       id: "q2",
-      audio: "/audio/u1/p1/q2.mp3",
       question:
         "It is considered ______ and insensitive to ask the disabled such questions.",
       optionA: "class",
@@ -22,7 +21,6 @@ export const en10u1p1ex1 = {
     },
     {
       id: "q3",
-      audio: "/audio/u1/p1/q3.mp3",
       question:
         "I found it hard to concentrate while hearing the loud ______ of the typewriter.",
       optionA: "crack",
@@ -31,7 +29,6 @@ export const en10u1p1ex1 = {
     },
     {
       id: "q4",
-      audio: "/audio/u1/p1/q4.mp3",
       question: "The state officer has become ______ about the affair.",
       optionA: "trunk",
       optionB: "chunk",
@@ -39,7 +36,6 @@ export const en10u1p1ex1 = {
     },
     {
       id: "q5",
-      audio: "/audio/u1/p1/q5.mp3",
       question:
         "One of the games held at the party was to ______ bubbles.",
       optionA: "blow",
@@ -48,7 +44,6 @@ export const en10u1p1ex1 = {
     },
     {
       id: "q6",
-      audio: "/audio/u1/p1/q6.mp3",
       question: "To ______ almonds, you need to remove the outer skin first.",
       optionA: "blanch",
       optionB: "branch",
@@ -56,7 +51,6 @@ export const en10u1p1ex1 = {
     },
     {
       id: "q7",
-      audio: "/audio/u1/p1/q7.mp3",
       question: "The little boy wanted one more ______ of cheese.",
       optionA: "trunk",
       optionB: "chunk",
@@ -64,15 +58,14 @@ export const en10u1p1ex1 = {
     },
     {
       id: "q8",
-      audio: "/audio/u1/p1/q8.mp3",
-      question: '"How wonderful!" she ______ as she reached the destination.',
+      question:
+        '"How wonderful!" she ______ as she reached the destination.',
       optionA: "chilled",
       optionB: "trilled",
       correct: "B",
     },
     {
       id: "q9",
-      audio: "/audio/u1/p1/q9.mp3",
       question:
         "What he said today is just going to ______ the conflict between the two teams.",
       optionA: "trigger",
@@ -81,7 +74,6 @@ export const en10u1p1ex1 = {
     },
     {
       id: "q10",
-      audio: "/audio/u1/p1/q10.mp3",
       question:
         "He now has to rely on a ______. One of his legs was broken two weeks ago.",
       optionA: "clutch",
