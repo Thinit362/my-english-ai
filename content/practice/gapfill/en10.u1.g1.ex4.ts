20666070de01977a769e12118160b665c7cc2081
Thinit362 <133291441+Thinit362@users.noreply.github.com>
2025-11-30 10:54:33 +0700

    Update en10.u1.g1.ex4.ts

--- content/practice/gapfill/en10.u1.g1.ex4.ts
+++ content/practice/gapfill/en10.u1.g1.ex4.ts
@@ -1,9 +1,9 @@
 // content/practice/gapFill/en10.u1.g1.ex4.ts
-import type { GapFillDataset } from "@/components/games/GapFillGame";
 
-export const en10u1g1ex4: GapFillDataset = {
+export const en10u1g1ex4 = {
   id: "en10.u1.g1.ex4",
-  title: "Hiện tại đơn & Hiện tại tiếp diễn - Bài tập 4",
+  title:
+    "Bài tập 4: Điền vào chỗ trống dạng đúng của động từ đã cho.",
   instructionsEn:
     "Fill in the blanks with the correct forms of the verbs given. Use negative form if necessary. You can use a word twice.",
   instructionsVi:
@@ -11,48 +11,48 @@ export const en10u1g1ex4: GapFillDataset = {
   givenWords: ["have", "take out", "take", "split", "prepare", "shop", "do"],
   items: [
     {
-      id: "1",
+      id: "q1",
       sentence:
         "I usually ___ the bus to school, but this morning I'm walking.",
       answers: ["take"],
     },
     {
-      id: "2",
+      id: "q2",
       sentence: "My mom is not at home. She ___ for groceries now.",
       answers: ["is shopping"],
     },
     {
-      id: "3",
+      id: "q3",
       sentence:
         "I'm very busy, so I only ___ the laundry every Saturday morning.",
       answers: ["do"],
     },
     {
-      id: "4",
+      id: "q4",
       sentence:
         "They ___ housework among members in Ann's family. She has to do all the chores.",
       answers: ["don't split", "do not split"],
     },
     {
-      id: "5",
+      id: "q5",
       sentence:
         "Kate always ___ her dog fed by her neighbor every time she goes on business trips.",
       answers: ["has"],
     },
     {
-      id: "6",
+      id: "q6",
       sentence:
         "I always cook, but today is Women's Day, so my husband ___ dinner in the kitchen.",
       answers: ["is preparing"],
     },
     {
-      id: "7",
+      id: "q7",
       sentence:
         "The residents in my neighborhood ___ the garbage at 5 p.m. every day when the bin lorry comes.",
       answers: ["take out"],
     },
     {
-      id: "8",
+      id: "q8",
       sentence:
         "Kris's wrist was broken once, so now she rarely ___ the heavy lifting.",
       answers: ["does"],
