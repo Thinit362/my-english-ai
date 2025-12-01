d8bb750bf9347229dfbf4d85a088e38db3b7da1e
Thinit362 <133291441+Thinit362@users.noreply.github.com>
2025-12-01 22:04:48 +0700

    Create datasets.ts

---
+++ content/practice/vocab/datasets.ts
@@ -0,0 +1,6 @@
+import { en10u5p1ex4 } from "./en10.u5.p1.ex4";
+
+export const VOCAB_DATASETS: Record<string, any> = {
+  // ... các dataset cũ
+  "en10.u5.p1.ex4": en10u5p1ex4, // 👈 thêm dòng này
+};
