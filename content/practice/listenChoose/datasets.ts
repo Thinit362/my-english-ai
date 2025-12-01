0a57ca56079bf36c122d38fa0dcde8ecedb23d38
Thinit362 <133291441+Thinit362@users.noreply.github.com>
2025-12-01 08:39:53 +0700

    Update datasets.ts

--- content/practice/listenChoose/datasets.ts
+++ content/practice/listenChoose/datasets.ts
@@ -1,7 +1,17 @@
 // content/practice/listenChoose/datasets.ts
-import { en10u1p1ex1 } from "./en10.u1.p1.ex1";
 
-// đơn giản dùng any cho đỡ rắc rối type
+import { en10u1p1ex1 } from "./en10.u1.p1.ex1";
+import { en10u2p1ex1 } from "./en10.u2.p1.ex1";
+import { en10u3p1ex1 } from "./en10.u3.p1.ex1";
+
+// Map toàn bộ dataset listen-choose
 export const LISTEN_CHOOSE_DATASETS: Record<string, any> = {
+  // Unit 1
   "en10.u1.p1.ex1": en10u1p1ex1,
+
+  // Unit 2
+  "en10.u2.p1.ex1": en10u2p1ex1,
+
+  // Unit 3
+  "en10.u3.p1.ex1": en10u3p1ex1,
 };
