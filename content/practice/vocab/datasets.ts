ab2c3cbfe1aa025e111794fb890228f185ae2f1c
Thinit362 <133291441+Thinit362@users.noreply.github.com>
2025-12-02 22:29:36 +0700

    Update datasets.ts

--- content/practice/vocab/datasets.ts
+++ content/practice/vocab/datasets.ts
@@ -1,10 +1,12 @@
 import { en10u5p1ex4 } from "./en10.u5.p1.ex4";
 import { en10u7p3ex1 } from "./en10.u7.p3.ex1";
 import { en10u9p1ex1 } from "./en10.u9.p1.ex1";
+import { en10u10p3ex3 } from "./en10.u10.p3.ex3";
 
 export const VOCAB_DATASETS: Record<string, any> = {
   // ... các dataset cũ
   "en10.u5.p1.ex4": en10u5p1ex4, 
   "en10.u7.p3.ex1": en10u7p3ex1,
-  "en10.u9.p1.ex1": en10u9p1ex1,// 👈 thêm dòng này
+  "en10.u9.p1.ex1": en10u9p1ex1,
+  "en10.u10.p3.ex3": en10u10p3ex3, // 👈 thêm dòng này
 };
