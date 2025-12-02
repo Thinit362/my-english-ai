e211dec17bc1453fe206e20c763a28ff4e647f0e
Thinit362 <133291441+Thinit362@users.noreply.github.com>
2025-12-02 20:43:03 +0700

    Update datasets.ts

--- content/practice/listenChoose/datasets.ts
+++ content/practice/listenChoose/datasets.ts
@@ -5,6 +5,7 @@ import { en10u2p1ex1 } from "./en10.u2.p1.ex1";
 import { en10u3p1ex1 } from "./en10.u3.p1.ex1";
 import { en10u4p1ex1 } from "./en10.u4.p1.ex1";
 import { en10u6p1ex1 } from "./en10.u6.p1.ex1";
+import { en10u8p1ex2 } from "./en10.u8.p1.ex2";
 
 // Map toàn bộ dataset listen-choose
 export const LISTEN_CHOOSE_DATASETS: Record<string, any> = {
@@ -21,4 +22,6 @@ export const LISTEN_CHOOSE_DATASETS: Record<string, any> = {
   "en10.u4.p1.ex1": en10u4p1ex1,
   // Unit 6
   "en10.u6.p1.ex1": en10u6p1ex1,
+  // Unit 8 – Pronunciation, Bài tập 2
+  "en10.u8.p1.ex2": en10u8p1ex2,
 };
