e89edf7b424f2333c82c4e123f465b50a8d7b29e
Thinit362 <133291441+Thinit362@users.noreply.github.com>
2025-12-02 08:58:16 +0700

    Create en10.u7.p3.ex1.ts

---
+++ content/practice/recordCompare/en10.u7.p3.ex1.ts
@@ -0,0 +1,59 @@
+// content/practice/recordCompare/en10.u7.p3.ex1.ts
+
+export const en10u7p3ex1 = {
+  id: "en10.u7.p3.ex1",
+  title: "Bài tập 3: Luyện tập về trọng âm ở từ có hơn ba âm tiết",
+  note: "Nghe và thu âm để so sánh. Chú ý đến phần trọng âm in đậm của những từ có hơn 3 âm tiết.",
+
+  items: [
+    {
+      id: "s1",
+      sentence:
+        "Through the years they have made significant contributions to species conservation in the area.",
+      words: [
+        "significant",
+        "contributions",
+        "conservation",
+      ],
+    },
+    {
+      id: "s2",
+      sentence:
+        "Ecotourism can help stimulate financial growth in developing nations and strengthen the global economy.",
+      words: [
+        "Ecotourism",
+        "financial",
+        "economy",
+      ],
+    },
+    {
+      id: "s3",
+      sentence:
+        "Water contamination could damage the fragile ecology of the coral reefs.",
+      words: [
+        "contamination",
+        "ecology",
+      ],
+    },
+    {
+      id: "s4",
+      sentence:
+        "The label identifies the products that are least harmful to the environment.",
+      words: [
+        "identifies",
+        "environment",
+      ],
+    },
+    {
+      id: "s5",
+      sentence:
+        "Sustainable tourism activities have minimal impact on the environment and culture of the host community.",
+      words: [
+        "Sustainable",
+        "activities",
+        "environment",
+        "community",
+      ],
+    },
+  ],
+};
