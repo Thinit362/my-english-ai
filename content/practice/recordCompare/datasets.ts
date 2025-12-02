fb7d0d05d8b89c2d262d5dc6efb57f0667c53a25
Thinit362 <133291441+Thinit362@users.noreply.github.com>
2025-12-02 09:19:25 +0700

    Create datasets.ts

---
+++ content/practice/recordCompare/datasets.ts
@@ -0,0 +1,7 @@
+//content/practice/recordCompare/datasets.ts
+import { en10u7p3ex1 } from "./en10.u7.p3.ex1";
+
+export const RECORD_COMPARE_DATASETS: Record<string, any> = {
+  "en10.u5.p4.ex1": en10u5p4ex1,
+  "en10.u7.p3.ex1": en10u7p3ex1, // 👈 thêm dòng này
+};
