4e3d33f22e7a92e4c6f1f7b91fb81c4afbaba444
thinit362 <tothinbkh@gmail.com>
2025-12-02 20:18:47 +0700

    Remove recordCompare folder

--- content/practice/recordCompare/datasets.ts
+++
@@ -1,6 +0,0 @@
-//content/practice/recordCompare/datasets.ts
-import { en10u7p3ex1 } from "./en10.u7.p3.ex1";
-
-export const RECORD_COMPARE_DATASETS: Record<string, any> = {
-    "en10.u7.p3.ex1": en10u7p3ex1, // 👈 thêm dòng này
-};
