d912ba76c8d040e34945109967181f351675b8f6
Thinit362 <133291441+Thinit362@users.noreply.github.com>
2025-12-02 20:40:35 +0700

    Create en10.u8.p1.ex2.ts

---
+++ content/practice/listenChoose/en10.u8.p1.ex2.ts
@@ -0,0 +1,83 @@
+// content/practice/listenChoose/en10.u8.p1.ex2.ts
+
+export const en10u8p1ex2 = {
+  id: "en10.u8.p1.ex2",
+  title:
+    "Trọng âm câu - Bài tập 2: Nghe và xác định xem từ nằm trong chỗ trống được đọc nhấn mạnh hay không được đọc nhấn mạnh.",
+  items: [
+    {
+      id: "q1",
+      question: "The doctor advised ________ to start a new hobby.",
+      optionA: "stressed",
+      optionB: "unstressed",
+      correct: "B",
+    },
+    {
+      id: "q2",
+      question: "________ doesn't have enough money to buy the computer.",
+      optionA: "stressed",
+      optionB: "unstressed",
+      correct: "B",
+    },
+    {
+      id: "q3",
+      question:
+        "The man suddenly realised that his neighbour ________ watching him.",
+      optionA: "stressed",
+      optionB: "unstressed",
+      correct: "B",
+    },
+    {
+      id: "q4",
+      question: "Pink and yellow are my ________ colours.",
+      optionA: "stressed",
+      optionB: "unstressed",
+      correct: "A",
+    },
+    {
+      id: "q5",
+      question:
+        "The _________ winds blew down the two ancient trees during the storm.",
+      optionA: "stressed",
+      optionB: "unstressed",
+      correct: "A",
+    },
+    {
+      id: "q6",
+      question: "I could never have passed that exam without your ________.",
+      optionA: "stressed",
+      optionB: "unstressed",
+      correct: "A",
+    },
+    {
+      id: "q7",
+      question: "Peter always takes notice of what ________ children say.",
+      optionA: "stressed",
+      optionB: "unstressed",
+      correct: "B",
+    },
+    {
+      id: "q8",
+      question:
+        "Patrick will ________ pass his driving test if he doesn't arrange some more lessons.",
+      optionA: "stressed",
+      optionB: "unstressed",
+      correct: "A",
+    },
+    {
+      id: "q9",
+      question: "You must do exactly what ________ teacher tells you.",
+      optionA: "stressed",
+      optionB: "unstressed",
+      correct: "B",
+    },
+    {
+      id: "q10",
+      question:
+        "Unfortunately, this television set has a ________ on the screen.",
+      optionA: "stressed",
+      optionB: "unstressed",
+      correct: "A",
+    },
+  ],
+};
