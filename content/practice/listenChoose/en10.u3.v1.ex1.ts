2a5b5b0d54c4605f6c0e905508a4e3b9b198b19e
Thinit362 <133291441+Thinit362@users.noreply.github.com>
2025-12-04 10:03:55 +0700

    Create en10.u3.v1.ex1.ts

---
+++ content/practice/listenChoose/en10.u3.v1.ex1.ts
@@ -0,0 +1,96 @@
+// content/practice/listenChoose/en10.u3.v1.ex1.ts
+
+export const en10u3v1ex1 = {
+  id: "en10.u3.v1.ex1",
+  title: "Bài tập 1: Chọn đáp án đúng điền vào chỗ trống.",
+  // 👇 ẩn nút nghe, chỉ làm trắc nghiệm
+  hideAudio: true,
+  items: [
+    {
+      id: "q1",
+      question:
+        "They are producing more and more ________ in the USA and they will soon spread to all parts of the world.",
+      optionA: "real TV shows",
+      optionB: "reality TV shows",
+      optionC: "actuality TV shows",
+      correct: "B",
+    },
+    {
+      id: "q2",
+      question:
+        "If you have any ________, please send them to this address.",
+      optionA: "comments",
+      optionB: "concerts",
+      optionC: "audience",
+      correct: "A",
+    },
+    {
+      id: "q3",
+      question: "They’re very good at cake ________.",
+      optionA: "perform",
+      optionB: "decoration",
+      optionC: "location",
+      correct: "B",
+    },
+    {
+      id: "q4",
+      question: "Heavy snow ________ the start of the competition.",
+      optionA: "delayed",
+      optionB: "performed",
+      optionC: "identified",
+      correct: "A",
+    },
+    {
+      id: "q5",
+      question: "I hope Anna will go through to the ________ with Jane.",
+      optionA: "final",
+      optionB: "semi-final",
+      optionC: "Both are correct.",
+      correct: "C",
+    },
+    {
+      id: "q6",
+      question:
+        "Nicholas Sparks is a ________ author. People love and respect him for his novels and personalities.",
+      optionA: "notorious",
+      optionB: "renowned",
+      optionC: "remarked",
+      correct: "B",
+    },
+    {
+      id: "q7",
+      question:
+        "I didn’t enjoy the movie because I couldn’t ________ with any of the main characters.",
+      optionA: "identify",
+      optionB: "search",
+      optionC: "hesitate",
+      correct: "A",
+    },
+    {
+      id: "q8",
+      question: "I’d love to see the band perform ________.",
+      optionA: "lively",
+      optionB: "live",
+      optionC: "lived",
+      correct: "B",
+    },
+    {
+      id: "q9",
+      question:
+        "These two artists have never ________ together before.",
+      optionA: "performer",
+      optionB: "performance",
+      optionC: "performed",
+      correct: "C",
+    },
+    {
+      id: "q10",
+      question:
+        "She went to the garden in ________ of her grandma.",
+      optionA: "look",
+      optionB: "judge",
+      optionC: "search",
+      correct: "C",
+    },
+  ],
+};
