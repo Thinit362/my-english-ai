0781d7c887d8fe965679e419307058849f957c48
Thinit362 <133291441+Thinit362@users.noreply.github.com>
2025-12-05 22:18:15 +0700

    Update datasets.ts

--- content/practice/vocab/datasets.ts
+++ content/practice/vocab/datasets.ts
@@ -5,6 +5,7 @@ import { en10u10p3ex3 } from "./en10.u10.p3.ex3";
 import { u6V1Ex1Data } from "./en10.u6.v1.ex1";
 import { u8V1Ex1Data } from "./en10.u8.v1.ex1";
 import { u9V1Ex1Data } from "./en10.u9.v1.ex1";
+import { u10V1Ex1Data } from "./en10.u10.v1.ex1";
 
 export const VOCAB_DATASETS: Record<string, any> = {
   // ... các dataset cũ
@@ -14,5 +15,6 @@ export const VOCAB_DATASETS: Record<string, any> = {
   "en10.u10.p3.ex3": en10u10p3ex3,
   "en10.u6.v1.ex1": u6V1Ex1Data,
   "en10.u8.v1.ex1": u8V1Ex1Data,
-  "en10.u9.v1.ex1": u9V1Ex1Data,// 👈 thêm dòng này
+  "en10.u9.v1.ex1": u9V1Ex1Data,
+  "en10.u10.v1.ex1": u10V1Ex1Data,// 👈 thêm dòng này
 };
