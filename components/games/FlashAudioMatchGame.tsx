ce188a273d6ef48da6eef1308edb50ad74f4f236
Thinit362 <133291441+Thinit362@users.noreply.github.com>
2025-12-05 21:25:26 +0700

    Update FlashAudioMatchGame.tsx

--- components/games/FlashAudioMatchGame.tsx
+++ components/games/FlashAudioMatchGame.tsx
@@ -8,6 +8,7 @@ import {
 import { u3V2Ex2Data } from "@/content/practice/vocab/en10.u3.v2.ex2";
 import { u4V1Ex1Data } from "@/content/practice/vocab/en10.u4.v1.ex1";
 import { u5V1Ex1Data } from "@/content/practice/vocab/en10.u5.v1.ex1";
+import { u6V1Ex1Data } from "@/content/practice/vocab/en10.u6.v1.ex1";
 
 interface Props {
   datasetId: string; // nhận id để chọn bộ dữ liệu
@@ -19,6 +20,7 @@ const DATASETS: Record<string, VocabTriple[]> = {
   "en10.u3.v2.ex2": u3V2Ex2Data,
   "en10.u4.v1.ex1": u4V1Ex1Data,
   "en10.u5.v1.ex1": u5V1Ex1Data,
+  "en10.u6.v1.ex1": u6V1Ex1Data,
 };
 
 type CardKind = "audio" | "meaning" | "type";
