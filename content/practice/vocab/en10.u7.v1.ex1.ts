ad69ec351bd799291b0af5cebfd92852fa505448
Thinit362 <133291441+Thinit362@users.noreply.github.com>
2025-12-05 21:34:53 +0700

    Create en10.u7.v1.ex1.ts

---
+++ content/practice/vocab/en10.u7.v1.ex1.ts
@@ -0,0 +1,76 @@
+// content/practice/vocab/en10.u7.v1.ex1.ts
+
+import { VocabTriple } from "./en10.u1.v1.ex1";
+
+export const u7V1Ex1Data: VocabTriple[] = [
+  {
+    id: "aim",
+    en: "aim",
+    vi: "mục tiêu; đặt ra mục tiêu",
+    typeLabel: "Noun / Verb (n.; v.)",
+    audioText: "aim",
+  },
+  {
+    id: "commit",
+    en: "commit",
+    vi: "cam kết",
+    typeLabel: "Verb (v.)",
+    audioText: "commit",
+  },
+  {
+    id: "economic",
+    en: "economic",
+    vi: "thuộc về kinh tế",
+    typeLabel: "Adjective (adj.)",
+    audioText: "economic",
+  },
+  {
+    id: "economy",
+    en: "economy",
+    vi: "nền kinh tế",
+    typeLabel: "Noun (n.)",
+    audioText: "economy",
+  },
+  {
+    id: "essential",
+    en: "essential",
+    vi: "cần thiết; cấp thiết",
+    typeLabel: "Adjective (adj.)",
+    audioText: "essential",
+  },
+  {
+    id: "expert",
+    en: "expert",
+    vi: "thuộc về chuyên môn",
+    typeLabel: "Adjective (adj.)",
+    audioText: "expert",
+  },
+  {
+    id: "invest",
+    en: "invest",
+    vi: "đầu tư",
+    typeLabel: "Verb (v.)",
+    audioText: "invest",
+  },
+  {
+    id: "peacekeeping",
+    en: "peacekeeping",
+    vi: "gìn giữ hòa bình",
+    typeLabel: "Adjective (adj.)",
+    audioText: "peacekeeping",
+  },
+  {
+    id: "poverty",
+    en: "poverty",
+    vi: "tình trạng nghèo đói",
+    typeLabel: "Noun (n.)",
+    audioText: "poverty",
+  },
+  {
+    id: "vaccinate",
+    en: "vaccinate",
+    vi: "tiêm vắc-xin",
+    typeLabel: "Verb (v.)",
+    audioText: "vaccinate",
+  },
+];
