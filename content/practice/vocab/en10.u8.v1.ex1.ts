c6ef62dbb0bbdf42a1352ba66ed43b5aeaf1d4d7
Thinit362 <133291441+Thinit362@users.noreply.github.com>
2025-12-05 21:54:31 +0700

    Create en10.u8.v1.ex1.ts

---
+++ content/practice/vocab/en10.u8.v1.ex1.ts
@@ -0,0 +1,76 @@
+// content/practice/vocab/en10.u8.v1.ex1.ts
+
+import { VocabTriple } from "./en10.u1.v1.ex1";
+
+export const u8V1Ex1Data: VocabTriple[] = [
+  {
+    id: "blended-learning",
+    en: "blended learning",
+    vi: "học tập kết hợp",
+    typeLabel: "Noun phrase (n. phr.)",
+    audioText: "blended learning",
+  },
+  {
+    id: "online learning",
+    en: "online learning",
+    vi: "học trực tuyến",
+    typeLabel: "Noun phrase (n. phr.)",
+    audioText: "online learning",
+  },
+  {
+    id: "face-to-face learning",
+    en: "face-to-face learning",
+    vi: "học trực tiếp (mặt-đối-mặt)",
+    typeLabel: "Noun phrase (n. phr.)",
+    audioText: "face to face learning",
+  },
+  {
+    id: "communicate",
+    en: "communicate",
+    vi: "giao tiếp, trao đổi",
+    typeLabel: "Verb (v.)",
+    audioText: "communicate",
+  },
+  {
+    id: "search for",
+    en: "search for",
+    vi: "tìm kiếm",
+    typeLabel: "Verb phrase (v. phr.)",
+    audioText: "search for",
+  },
+  {
+    id: "upload",
+    en: "upload",
+    vi: "tải lên",
+    typeLabel: "Verb (v.)",
+    audioText: "upload",
+  },
+  {
+    id: "material",
+    en: "material",
+    vi: "tài liệu",
+    typeLabel: "Noun (n.)",
+    audioText: "material",
+  },
+  {
+    id: "digital",
+    en: "digital",
+    vi: "kỹ thuật số, số hóa",
+    typeLabel: "Adjective (adj.)",
+    audioText: "digital",
+  },
+  {
+    id: "distraction",
+    en: "distraction",
+    vi: "sự xao nhãng, sự mất tập trung",
+    typeLabel: "Noun (n.)",
+    audioText: "distraction",
+  },
+  {
+    id: "control",
+    en: "control",
+    vi: "sự kiểm soát; kiểm soát",
+    typeLabel: "Noun / Verb (n.; v.)",
+    audioText: "control",
+  },
+];
