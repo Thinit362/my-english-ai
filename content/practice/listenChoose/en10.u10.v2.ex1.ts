a29b796bbe766d6c1f47ee9e156a61a344d18f33
Thinit362 <133291441+Thinit362@users.noreply.github.com>
2025-12-06 22:17:52 +0700

    Create en10.u10.v2.ex1.ts

---
+++ content/practice/listenChoose/en10.u10.v2.ex1.ts
@@ -0,0 +1,100 @@
+// content/practice/listenChoose/en10.u10.v2.ex1.ts
+
+export const en10u10v2ex1 = {
+  id: "en10.u10.v2.ex1",
+  title: "Unit 10 – Vocabulary 2: Chọn đáp án đúng điền vào chỗ trống.",
+  // Ẩn nút nghe, chỉ làm trắc nghiệm giống Unit 9
+  hideAudio: true,
+  items: [
+    {
+      id: "q1",
+      question:
+        "Untreated toxic wastewater dumped into the ocean has caused ________ fish deaths.",
+      optionA: "mass",
+      optionB: "profit",
+      optionC: "souvenir",
+      correct: "A",
+    },
+    {
+      id: "q2",
+      question:
+        "________ is a form of tourism that involves tens of thousands of people going to the same place, often at the same time of year.",
+      optionA: "Local tourism",
+      optionB: "Mass tourism",
+      optionC: "Travel brochure",
+      correct: "B",
+    },
+    {
+      id: "q3",
+      question:
+        "A ________ is a very useful book when you travel to new places.",
+      optionA: "travel brochure",
+      optionB: "path",
+      optionC: "host",
+      correct: "A",
+    },
+    {
+      id: "q4",
+      question:
+        "Which country will be the ________ of the next Olympic Games?",
+      optionA: "mass",
+      optionB: "host",
+      optionC: "souvenir",
+      correct: "B",
+    },
+    {
+      id: "q5",
+      question:
+        "November is a good time to ________ deer in some countries.",
+      optionA: "hunt",
+      optionB: "protect",
+      optionC: "weave",
+      correct: "A",
+    },
+    {
+      id: "q6",
+      question:
+        "My brother goes to the ________ school just around the corner.",
+      optionA: "responsible",
+      optionB: "local",
+      optionC: "mass",
+      correct: "B",
+    },
+    {
+      id: "q7",
+      question:
+        "The ________ leads us down to the river.",
+      optionA: "path",
+      optionB: "souvenir",
+      optionC: "profit",
+      correct: "A",
+    },
+    {
+      id: "q8",
+      question:
+        "The company’s investments showed a ________ of 26% last year.",
+      optionA: "mass",
+      optionB: "profit",
+      optionC: "host",
+      correct: "B",
+    },
+    {
+      id: "q9",
+      question:
+        "Are we doing enough to ________ the environment?",
+      optionA: "protect",
+      optionB: "hunt",
+      optionC: "waste",
+      correct: "A",
+    },
+    {
+      id: "q10",
+      question:
+        "I bought this bracelet as a ________ of my trip to Greece.",
+      optionA: "trail",
+      optionB: "stalactite",
+      optionC: "souvenir",
+      correct: "C",
+    },
+  ],
+};
