088547d4a87c2fd209127991a28993d429a97761
Thinit362 <133291441+Thinit362@users.noreply.github.com>
2025-12-07 14:32:59 +0700

    Update datasets.ts

--- content/practice/dragFill/datasets.ts
+++ content/practice/dragFill/datasets.ts
@@ -5,6 +5,7 @@ import { en10u3g1ex1 } from "./en10.u3.g1.ex1";
 import { en10u3g1ex2 } from "./en10.u3.g1.ex2";
 import { en10u3g2ex1 } from "./en10.u3.g2.ex1";
 import { en10u3g2ex2 } from "./en10.u3.g2.ex2";
+import { en10u4g1ex1 } from "./en10.u4.g1.ex1";
 
 export const DRAG_FILL_DATASETS: Record<string, DragFillDataset> = {
   "en10.u1.v2.ex1": en10u1v2ex1,
@@ -12,5 +13,7 @@ export const DRAG_FILL_DATASETS: Record<string, DragFillDataset> = {
   "en10.u3.g1.ex1": en10u3g1ex1,
   "en10.u3.g1.ex2": en10u3g1ex2,
   "en10.u3.g2.ex1": en10u3g2ex1,
-  "en10.u3.g2.ex2": en10u3g2ex2,
+  "en10.u3.g2.ex2": en10u3g2ex2
+  "en10.u4.g1.ex1": en10u4g1ex1,
+  
 };
