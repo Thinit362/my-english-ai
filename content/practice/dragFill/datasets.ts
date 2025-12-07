5cfcc915ff258658591158e2e1d4357403cd799b
Thinit362 <133291441+Thinit362@users.noreply.github.com>
2025-12-07 16:31:59 +0700

    Update datasets.ts

--- content/practice/dragFill/datasets.ts
+++ content/practice/dragFill/datasets.ts
@@ -9,6 +9,7 @@ import { en10u4g1ex1 } from "./en10.u4.g1.ex1";
 import { en10u5g2ex1 } from "./en10.u5.g2.ex1";
 import { en10u6g1ex2 } from "./en10.u6.g1.ex2";
 import { en10u7g1ex1 } from "./en10.u7.g1.ex1";
+import { en10u8g1ex1 } from "./en10.u8.g1.ex1";
 
 export const DRAG_FILL_DATASETS: Record<string, DragFillDataset> = {
   "en10.u1.v2.ex1": en10u1v2ex1,
@@ -21,4 +22,5 @@ export const DRAG_FILL_DATASETS: Record<string, DragFillDataset> = {
   "en10.u5.g2.ex1": en10u5g2ex1,
   "en10.u6.g1.ex2": en10u6g1ex2,
   "en10.u7.g1.ex1": en10u7g1ex1,
+  "en10.u8.g1.ex1": en10u8g1ex1,
 };
