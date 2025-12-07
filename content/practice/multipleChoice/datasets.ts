8e615afaca9211a60774993f0f5948b4ceba127a
Thinit362 <133291441+Thinit362@users.noreply.github.com>
2025-12-07 16:15:01 +0700

    Update datasets.ts

--- content/practice/multipleChoice/datasets.ts
+++ content/practice/multipleChoice/datasets.ts
@@ -2,8 +2,10 @@
 import { MultipleChoiceDataset } from "@/components/games/MultipleChoiceGame";
 import { en10u5g1mc1 } from "./en10.u5.g1.mc1";
 import { en10u6g1mc1 } from "./en10.u6.g1.mc1";
+import { en10u7g1mc1 } from "./en10.u7.g1.mc1";
 
 export const MULTIPLE_CHOICE_DATASETS: Record<string, MultipleChoiceDataset> = {
   "en10.u5.g1.mc1": en10u5g1mc1,
   "en10.u6.g1.mc1": en10u6g1mc1,
+  "en10.u7.g1.mc1": en10u7g1mc1,
 };
