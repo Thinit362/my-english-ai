c31354b4f54e65e333336ce6b53e8113b4056906
Thinit362 <133291441+Thinit362@users.noreply.github.com>
2025-12-07 11:10:58 +0700

    Update datasets.ts

--- content/practice/dragFill/datasets.ts
+++ content/practice/dragFill/datasets.ts
@@ -1,8 +1,12 @@
 import { DragFillDataset } from "@/components/games/DragFillGame";
 import { en10u1v2ex1 } from "./en10.u1.v2.ex1";
-import { en10u2v2ex1 } from "./en10.u2.v2.ex1"; // 👈 thêm dòng này
+import { en10u2v2ex1 } from "./en10.u2.v2.ex1"; 
+import { en10u3g1ex1 } from "./en10.u3.g1.ex1";
+import { en10u3g1ex2 } from "./en10.u3.g1.ex2";
 
 export const DRAG_FILL_DATASETS: Record<string, DragFillDataset> = {
   "en10.u1.v2.ex1": en10u1v2ex1,
-  "en10.u2.v2.ex1": en10u2v2ex1,   // 👈 thêm key mới
+  "en10.u2.v2.ex1": en10u2v2ex1,   
+  "en10.u3.g1.ex1": en10u3g1ex1,
+  "en10.u3.g1.ex2": en10u3g1ex2,
 };
