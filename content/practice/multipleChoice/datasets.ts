e47d9415f17c0706495d07252ebf7aff3ce156b6
Thinit362 <133291441+Thinit362@users.noreply.github.com>
2025-12-07 14:57:04 +0700

    Create datasets.ts

---
+++ content/practice/multipleChoice/datasets.ts
@@ -0,0 +1,7 @@
+// content/practice/multipleChoice/datasets.ts
+import { MultipleChoiceDataset } from "@/components/games/MultipleChoiceGame";
+import { en10u5g1mc1 } from "./en10.u5.g1.mc1";
+
+export const MULTIPLE_CHOICE_DATASETS: Record<string, MultipleChoiceDataset> = {
+  "en10.u5.g1.mc1": en10u5g1mc1,
+};
