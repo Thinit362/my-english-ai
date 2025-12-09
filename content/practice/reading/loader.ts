cd47c822228af6c7b077821b2cfba9c9c786e079
Thinit362 <133291441+Thinit362@users.noreply.github.com>
2025-12-09 15:27:02 +0700

    Update loader.ts

--- content/practice/reading/loader.ts
+++ content/practice/reading/loader.ts
@@ -8,8 +8,9 @@ import en10_u5_read1 from "./en10.u5.read1";
 import en10_u6_read1 from "./en10.u6.read1";
 import en10_u7_read1 from "./en10.u7.read1";
 import en10_u8_read1 from "./en10.u8.read1";
+import en10_u9_read1 from "./en10.u9.read1";
 
-const datasets: ReadingLesson[] = [en10_u1_read1, en10_u2_read1,en10_u3_read1, en10_u4_read1,en10_u5_read1,en10_u6_read1,en10_u7_read1,en10_u8_read1];
+const datasets: ReadingLesson[] = [en10_u1_read1, en10_u2_read1,en10_u3_read1, en10_u4_read1,en10_u5_read1,en10_u6_read1,en10_u7_read1,en10_u8_read1,en10_u9_read1]];
 
 export function getReadingByUnit(unit: number): ReadingLesson | undefined {
   return datasets.find((d) => d.unit === unit);
