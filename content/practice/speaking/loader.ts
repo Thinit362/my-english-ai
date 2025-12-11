6cc356853c80bc5d5772cba6651290cde53d55bf
Thinit362 <133291441+Thinit362@users.noreply.github.com>
2025-12-11 20:48:33 +0700

    Update loader.ts

--- content/practice/speaking/loader.ts
+++ content/practice/speaking/loader.ts
@@ -1,13 +1,14 @@
 // content/practice/speaking/loader.ts
 import type { SpeakingContent, SpeakingLesson } from "./types";
 import { speakingUnit1 } from "./unit1";
-import { speakingUnit2 } from "./unit2"
+import { speakingUnit2 } from "./unit2";
+import { speakingUnit3 } from "./unit3";
 
 // Sau này bạn chỉ cần import thêm unit2, unit3... rồi thêm vào map dưới
 const speakingContent: SpeakingContent = {
   1: speakingUnit1,
   2: speakingUnit2,
-  // 3: speakingUnit3,
+  3: speakingUnit3,
 };
 
 export function getSpeakingByUnit(unit: number): SpeakingLesson | undefined {
