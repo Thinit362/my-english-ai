065ce1e3626300874f0257739a318b3a4a92e893
Thinit362 <133291441+Thinit362@users.noreply.github.com>
2025-12-16 22:36:09 +0700

    Update loader.ts

--- content/practice/listening/loader.ts
+++ content/practice/listening/loader.ts
@@ -4,12 +4,12 @@ import type { ListeningLesson } from "./types";
 // Import từng bài nghe (Unit 1, Unit 2, ...)  
 // Hiện tại bạn mới có Unit 1
 import en10_u1_listen1 from "./en10.u1.listen1";
+import en10_u2_listen1 from "./en10.u2.listen1";
 
 // Mảng chứa toàn bộ bài luyện nghe
 const listeningData: ListeningLesson[] = [
   en10_u1_listen1,
-  // Sau này bạn chỉ cần thêm:
-  // en10_u2_listen1,
+  en10_u2_listen1,
   // en10_u3_listen1,
   // ...
 ];
