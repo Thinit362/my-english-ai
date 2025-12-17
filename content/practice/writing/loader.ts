48e9b1bdd1da04e044c4e71c1a3ff74ef8200de2
Thinit362 <133291441+Thinit362@users.noreply.github.com>
2025-12-17 22:30:11 +0700

    Update loader.ts

--- content/practice/writing/loader.ts
+++ content/practice/writing/loader.ts
@@ -1,20 +1,8 @@
+import { writingUnit1 } from "./unit1";
 import type { WritingLesson } from "./types";
 
-import u1w1 from "./en10.u1.write1";
-import u1w2 from "./en10.u1.write2";
-
-const lessons: WritingLesson[] = [u1w1, u1w2];
+const lessons: WritingLesson[] = [writingUnit1];
 
 export function getWritingByUnit(unit: number): WritingLesson | undefined {
-  // gộp các page của cùng unit (vì unit có nhiều file write1, write2…)
-  const sameUnit = lessons.filter((l) => l.unit === unit);
-  if (sameUnit.length === 0) return undefined;
-
-  // lấy meta từ file đầu, gộp theory + exercises theo thứ tự file
-  const base = sameUnit[0];
-  return {
-    ...base,
-    theory: sameUnit.flatMap((x) => x.theory ?? []),
-    exercises: sameUnit.flatMap((x) => x.exercises),
-  };
+  return lessons.find((l) => l.unit === unit);
 }
