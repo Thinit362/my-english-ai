2d0515064f4edf6061db18923d7b490008f12758
Thinit362 <133291441+Thinit362@users.noreply.github.com>
2025-12-19 22:40:13 +0700

    Update loader.ts

--- content/practice/listening/loader.ts
+++ content/practice/listening/loader.ts
@@ -10,6 +10,7 @@ import en10_u4_listen1 from "./en10.u4.listen1";
 import en10_u5_listen1 from "./en10.u5.listen1";
 import en10_u6_listen1 from "./en10.u6.listen1";
 import en10_u7_listen1 from "./en10.u7.listen1";
+import en10_u9_listen1 from "./en10.u9.listen1";
 // Mảng chứa toàn bộ bài luyện nghe
 const listeningData: ListeningLesson[] = [
   en10_u1_listen1,
@@ -18,7 +19,8 @@ const listeningData: ListeningLesson[] = [
   en10_u4_listen1,
   en10_u5_listen1,
   en10_u6_listen1,
-   en10_u7_listen1,
+  en10_u7_listen1,
+  en10_u9_listen1,
   // en10_u3_listen1,
   // ...
 ];
