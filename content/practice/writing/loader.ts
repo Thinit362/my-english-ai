ee7525805cb57cf3f49e8f3d2224700ecd76f42c
Thinit362 <133291441+Thinit362@users.noreply.github.com>
2025-12-20 16:06:32 +0700

    Update loader.ts

--- content/practice/writing/loader.ts
+++ content/practice/writing/loader.ts
@@ -1,9 +1,10 @@
 //content/practice/writing/loader.ts
 import { writingUnit1 } from "./unit1";
 import { writingUnit2 } from "./unit2";
+import { writingUnit2 } from "./unit2";
 import type { WritingLesson } from "./types";
 
-const lessons: WritingLesson[] = [writingUnit1,writingUnit2];
+const lessons: WritingLesson[] = [writingUnit1, writingUnit2, writingUnit3];
 
 export function getWritingByUnit(unit: number): WritingLesson | undefined {
   return lessons.find((l) => l.unit === unit);
