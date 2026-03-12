e0ac168d292c8929690ad8a0d3360fe24d3c0fc1
Thinit362 <133291441+Thinit362@users.noreply.github.com>
2026-03-12 21:06:02 +0700

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,9 @@
 export default function Footer() {
 return (
 <footer className="footer">
-MCVT Innovators – THPT Hải An
+Trường THPT Hải An, thành phố Hải Phòng
+Địa chỉ: 10/1177 Ngô Gia Tự, Nam Hải 2, Hải An, Hải Phòng
+ĐT: 0225.268 6343 - email: thpt-haian@haiphong.edu.vn
 </footer>
 );
 }
